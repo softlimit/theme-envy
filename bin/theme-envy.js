d9b89c91f83a8a2a2af18eda3d5ef4d675cb2970
Olivia <info@obeaty.com>
2023-03-10 12:33:53 -0600

    add help descriptions, move to correct file

--- bin/theme-envy.js
+++ bin/theme-envy.js
@@ -1,57 +1,115 @@
 #!/usr/bin/env node
 
-'use strict'
-
-/*
-* Controls the other theme- files in this /bin directory
-* Execute from client theme where softlimit-framework-2 is installed as a package with:
-* npx theme <command> ...
-* npx theme build development|production --watch|-w
-* npx theme ignore none|push|pull
-* npx theme pull-json // retrieves all JSON files from a theme and pulls into src
-* npx theme clean // empties the dist folder
-* npx theme new feature|element ...
-* npx theme prep // used during npm run start to determine whether we need a fresh build
-*/
-
-const yargs = require('yargs')
+const commander = require('commander')
+const program = new commander.Command()
 const chalk = require('chalk')
-// eslint-disable-next-line no-unused-vars
-const argv = yargs(process.argv.slice(2))
-  .option('watch', {
-    alias: 'w',
-    type: 'boolean',
-    default: false
+const themeEnvyCommands = require('#Root/theme-envy-commands.js')
+
+const scriptMessage = (scriptName) => {
+  console.log(
+    chalk.green.bold('Starting Softlimit Theme Envy script'),
+    chalk.bgGreen(`theme-envy ${scriptName}\n`)
+  )
+}
+
+program
+  .description('Theme Envy CLI Tools')
+  .addHelpText('beforeAll', `
+${chalk.green.bold('Welcome to Shopify Theme Envy <3')}
+  `)
+
+program
+  .command('build')
+  .description('Build Shopify theme dist from src in either production or development context with watch process option for serving')
+  .usage('npx theme-envy development|production -w|--watch')
+  .addArgument(new commander.Argument('[env]', 'Specify the build environment to run').choices(['development', 'production']))
+  .option('-w, --watch', 'Watch for changed files and update dist')
+  .action((env, options, command) => {
+    scriptMessage(command.name())
+    themeEnvyCommands.build(env, options)
   })
-  .option('target', {
-    alias: 't',
-    describe: 'init: Path to target destination of src folder and config files',
-    type: 'string',
-    default: './',
-    nargs: 1
+
+program
+  .command('clean')
+  .description('Clear dist folder contents')
+  .usage('npx theme-envy clean')
+  .action((command) => {
+    scriptMessage(command.name())
+    themeEnvyCommands.clean()
   })
-  .option('example-feature', {
-    alias: 'ef',
-    decribe: 'init: Add example feature to _features when copying the src structure'
+
+program
+  .command('convert')
+  .description('Convert an existing Shopify theme to Theme Envy directory structure')
+  .usage('npx theme-envy convert [source]')
+  .argument('[source]', 'Specify the path to your theme source directory to process, defaults to project root ./src directory if not provided')
+  .action((source, command) => {
+    scriptMessage(command.name())
+    themeEnvyCommands.convert(source)
   })
-  .command('$0 <script> [args]', 'Theme Envy Scripts', {
-    script: {
-      description: 'Available scripts: ["build", "clean", "ignore", "prep", "pull-json", "new"]. Each script has additional info with the "--help" or "-h" flag.',
-      type: 'string'
-    },
-    args: {
-      description: 'Script arguments',
-      type: 'array'
+
+program
+  .command('dev')
+  .description('Start development process and sync with Shopify using the Shopify CLI')
+  .action((command) => {
+    scriptMessage(command.name())
+    themeEnvyCommands.dev()
+  })
+
+program
+  .command('ignore')
+  .action((command) => {
+    scriptMessage(command.name())
+    themeEnvyCommands.ignore()
+  })
+
+program
+  .command('import')
+  .description('Import an existing Shopify theme into a destination directory (default: "./src")')
+  .usage('npx theme-envy import [source]')
+  .argument('[source]', 'Specify the path to your theme source directory to process, defaults to project root ./src directory if not provided')
+  .option('-c, --convert', 'Convert theme sections to _features, add hooks, and install theme-envy feature on import')
+  .action((source, options, command) => {
+    scriptMessage(command.name())
+    themeEnvyCommands.import(source, options)
+  })
+
+program
+  .command('init')
+  .description('Create skeleton structure for src folder into a destination directory (default: project root "./")')
+  .usage('npx theme-envy init [source] -e|--example')
+  .argument('[source]', 'Path to target directory to init shopify theme envy files in, defaults to current user directory')
+  .option('-e, --example', 'Output example feature structure and dummy files with readme documentation in each subdirectory')
+  .action((source, options, command) => {
+    scriptMessage(command.name())
+    themeEnvyCommands.init(source, options)
+  })
+
+program
+  .command('new')
+  .description('Create named directory in _features or _elements with starter files to build new feature|element')
+  .usage('npx theme-envy <type> <name> [include]')
+  .addArgument(new commander.Argument('<type>', 'Define the type of scaffold to create').choices(['feature', 'element']))
+  .argument('<name>', 'Handleized name for the new element|feature')
+  .argument('[include]', 'Comma-separated list of starter directories and files to include in your scaffold. Defaults to all if not provided.')
+  .action((type, name, include, options, command) => {
+    scriptMessage(command.name())
+    if (name.includes(',') || name.includes(' ')) {
+      console.error(
+        chalk.red('Invalid Name:'),
+        'Name argument should only include text and dashes, no commas or spaces. Rename with handle version to continue.'
+      )
+      return
     }
-  },
-  (argv) => {
-    console.log(
-      chalk.green.bold('Starting Softlimit Theme Envy script'),
-      chalk.bgGreen(`theme-envy ${argv.script}\n`)
-    )
-    const themeEnvyCommands = require('#Root/theme-envy-commands.js')
-    themeEnvyCommands[argv.script]({ argv })
+    themeEnvyCommands.new(type, name, include)
   })
-  .help('h')
-  .showHelpOnFail(true)
-  .argv
+
+program
+  .command('pull-json')
+  .description('Pull json template, section, and settings_data files from theme using Shopify CLI')
+  .action((command) => {
+    scriptMessage(command.name())
+    themeEnvyCommands['pull-json']()
+  })
+
+program.parse()
