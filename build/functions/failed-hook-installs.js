61a85631dd8c529ade0f35071aad128c043a6f75
Olivia <info@obeaty.com>
2023-03-16 13:03:22 -0500

    refactor from forEach to filter

--- build/functions/failed-hook-installs.js
+++ build/functions/failed-hook-installs.js
@@ -2,13 +2,8 @@ const chalk = require('chalk')
 const logSymbols = require('#LogSymbols')
 
 module.exports = function() {
-  const unusedHookInstalls = []
-  Object.keys(process.build.hooks).forEach(hook => {
-    if (!process.build.hooks[hook].replaced) {
-      unusedHookInstalls.push(hook)
-    }
-  })
-  if (unusedHookInstalls.length === 0) return
+  const unusedHookInstalls = Object.keys(process.build.hooks).filter(hook => !process.build.hooks[hook].replaced)
+  if (!unusedHookInstalls || unusedHookInstalls.length === 0) return
   console.error(`
 ${logSymbols.error} ${chalk.red.bold('Error:')}
 The following hook injections failed because there is no corresponding hook tag in the theme:
