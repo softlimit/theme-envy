0164f50c4ae80cddc73a2a2616ab1a124d2dc8a5
Caleb Curtis <caleb@softlimit.com>
2023-03-16 15:53:52 -0400

    change to single progress bar can be made much smoother

--- build/requires/globals/build-progress.js
+++ build/requires/globals/build-progress.js
@@ -1,19 +1,12 @@
 const cliProgress = require('cli-progress')
 process.build = process.build || {}
-const container = new cliProgress.MultiBar({
-  clearOnComplete: false,
-  hideCursor: true,
-  format: ' {bar} {percentage}%',
-}, cliProgress.Presets.shades_classic)
 
 process.build.progress = {
-  container,
-  bar: container.create(1, 0),
+  bar: new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic),
   total: 0,
   addToTotal: (amount) => {
     process.build.progress.total += (amount || 1)
-    if (process.build.progress.bar.isActive) process.build.progress.bar.setTotal(process.build.progress.total)
+    process.build.progress.bar.setTotal(process.build.progress.total)
   }
 }
-process.build.progress.addToTotal(12)
-process.build.progress.bar.start(process.build.progress.total, 0)
+process.build.progress.bar.start(12, 0)
