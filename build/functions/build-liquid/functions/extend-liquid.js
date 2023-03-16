02e35e896449e1f2a2c4117c72332df3b4902f69
Olivia <info@obeaty.com>
2023-03-16 12:57:26 -0500

    fix: extend hook content

--- build/functions/build-liquid/functions/extend-liquid.js
+++ build/functions/build-liquid/functions/extend-liquid.js
@@ -48,7 +48,7 @@ function replaceTag({ tag, source, filePath } = {}) {
     source = source.replace(replace, whitespace(replace, file))
   }
   if (action === 'hook') {
-    const replacedContent = process.build.hooks[name] ? extendLiquid({ source: process.build.hooks[name], filePath }) : ''
+    const replacedContent = process.build.hooks[name] ? extendLiquid({ source: process.build.hooks[name].content, filePath }) : ''
     if (process.build.hooks[name]) process.build.hooks[name].replaced = true
     source = source.replace(replace, replacedContent)
   }
